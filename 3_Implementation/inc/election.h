5f00a646f3b1da8980afe0370274c73f680ac55d
harshithasnayaka <89700410+harshithasnayaka@users.noreply.github.com>
2021-09-07 16:49:58 +0530

    Update election.h

--- 3_Implementation/inc/election.h
+++ 3_Implementation/inc/election.h
@@ -1,3 +1,13 @@
+**
+ * @file Election.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
