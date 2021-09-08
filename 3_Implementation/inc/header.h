f05759118f520f2b235bf998764cf5cce5d226ae
harshithasnayaka <89700410+harshithasnayaka@users.noreply.github.com>
2021-09-08 19:38:23 +0530

    Update header.h

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,3 +1,13 @@
+/**
+ * @file header.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __HEADERS_H__
 #define __HEADERS_H__
 #include <stdio.h>
